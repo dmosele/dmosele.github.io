d48b9d9596d81c6eed74f32193cefb3dc8fd8b04
dmosele <65730666+dmosele@users.noreply.github.com>
2021-08-11 09:12:45 -0400

    dsf update 081121

--- DataScienceFoundations/_build/jupyter_execute/Intro_to_Python.py
+++ DataScienceFoundations/_build/jupyter_execute/Intro_to_Python.py
@@ -5,6 +5,29 @@
 
 # Welcome to Python! There are a couple of ways to get started using Python. One is to install it directly on your computer using something like Anaconda. The other way is to use it in the cloud with a service like Google Colab. Either way is fine for now, so let's get started.
 
+# ## Google Colab
+# 
+# The nice thing about Google Colab is that it is available basically anywhere you have an internet browser. There's nothing to install. All you need to do is go to https://colab.research.google.com and create a new notebook.
+# 
+# The downside, at least for me, is that it is not intuitive how to import data into Colab. If you want to import data into colab, you can use the following code:
+
+# In[1]:
+
+
+import pandas as pd
+from google.colab import files
+import io
+
+uploaded = files.upload()
+df2 = pd.read_csv(io.BytesIO(uploaded['name_of_file.csv']))
+
+
+# ## Anaconda
+# 
+# The program Anaconda is Python equipped with a bunch of tools and an easy way to add new tools to it. The downside is that it is software to be installed, but the upside is that it is free!
+# 
+# You can download it from https://www.anaconda.com/products/individual and install it. Once it is installed you can install and then launch Jupyter Notebook from the Anaconda Navigator.
+
 # In[ ]:
 
 
--- DataScienceFoundations/_build/jupyter_execute/Python Basic Mathematical Operations.py
+++ DataScienceFoundations/_build/jupyter_execute/Python Basic Mathematical Operations.py
@@ -43,6 +43,32 @@
 3**5
 
 
+# Most of the time, we will actually be using variables to represent numbers (and strings, lists, matrices, etc). Let's have a look at some computations when using variables.
+
+# In[6]:
+
+
+x = 5
+y = 3
+print(x+y,x*y,y**x,x/y,sep='\n')
+
+
+# If we want to play with a couple of more nuanced operations, using two forward slashes represents division and round to the nearest integer. The percent symbol is the modulus of a number, or the remainder when you divide one number by another.
+
+# In[7]:
+
+
+x//y
+
+
+# In[8]:
+
+
+x%y
+
+
+# Note that in my notebook, Python is remembering that I assigned x the value 5 and y the value 3.
+
 # In[ ]:
 
 
