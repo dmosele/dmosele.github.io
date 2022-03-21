3c786fe97fb82928ac93e16d16c4a202b6e1860b
dmosele <65730666+dmosele@users.noreply.github.com>
2022-03-21 17:34:39 -0400

    Add new chapter

---
+++ DataScienceFoundations/_build/jupyter_execute/Loops Functions and Conditions.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Loops, Functions, and Conditions
+
+# In this section, we will learn about one more data type, a list, as well as how to iterate. To be continued 
